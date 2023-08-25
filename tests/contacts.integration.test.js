9b0fafafc8dbb109b004b8025594c138597d072a
vipullahepatil <113188573+vipullahepatil@users.noreply.github.com>
2023-08-25 09:41:04 +0530

    Add files via upload

---
+++ tests/contacts.integration.test.js
@@ -0,0 +1,19 @@
+
+import request from 'supertest';
+import app from '../app'; 
+
+describe('Contacts API', () => {
+  it('should create a new contact', async () => {
+    const res = await request(app)
+      .post('/contacts')
+      .send({
+        name: 'VipulLahe',
+        email: 'vipullahepatil@gmail.com',
+        phoneNumbers: ['5208']
+      });
+
+    expect(res.status).toEqual(200);
+    expect(res.body.message).toEqual('Contact created successfully');
+  });
+
+});
